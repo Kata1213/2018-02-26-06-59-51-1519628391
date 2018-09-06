f091e41e214b539dbec2b18e2ca97e4ae3985a5e
Jingyue Li <jingyli@thoughtworks.com>
2018-09-06 20:01:14 +0800

    [Jingyue]finish practice 7

--- src/practice_7/class.js
+++ src/practice_7/class.js
@@ -1,1 +1,15 @@
 
+//should get display name with number
+"use strict";
+class Class{
+    constructor(klass){
+        this.number=klass;
+    }
+    getDisplayName(){
+        return `Class ${this.number}`;
+    }
+}
+
+export default Class;
+
+
