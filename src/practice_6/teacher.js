50c6c4c73bc5652b4f5fbd4366d1b3ab6679b9b1
Jingyue Li <jingyli@thoughtworks.com>
2018-09-06 19:34:55 +0800

    [Jingyue]finish practice 6

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,21 @@
+//should have field name, age and class number,should overwrite Person introduce, introduce with name
+
+"use strict";
+import Person from "../practice_3/person";
+class Teacher extends Person{
+    constructor(name,age,klass){
+        super(name,age);
+        this.klass=klass;
+    }
+
+    introduce(){
+        var res=`My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I `;
+         res += this.klass===undefined?`teach No Class.`:`teach Class ${this.klass}.`;
+         return res;
+    }
+}
+
+export default Teacher;
+
+
 
