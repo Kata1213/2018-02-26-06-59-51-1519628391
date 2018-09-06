66276ff118df835699ef6ed532b61ed885759c9c
Jingyue Li <jingyli@thoughtworks.com>
2018-09-06 19:14:52 +0800

    finish practice 4

--- src/practice_4/worker.js
+++ src/practice_4/worker.js
@@ -1,1 +1,18 @@
 
+
+//should overwrite Person introduce, introduce with name and age
+"use strict";
+import Person from "../practice_3/person";
+class Worker extends Person{
+    constructor(name,age){
+        super(name,age);
+    }
+
+    introduce(){
+        return `My name is ${this.name}. I am ${this.age} years old. I am a Worker. I have a job.`;
+    }
+}
+
+export default Worker;
+
+
